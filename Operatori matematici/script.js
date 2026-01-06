5bb5c1d8cc0742ee7d53b9130af6253214316399
angelorapana <angelorapana@icloud.com>
2026-01-06 17:48:36 +0100

    creazione funzione per bottone +

--- Operatori matematici/script.js
+++ Operatori matematici/script.js
@@ -5,6 +5,15 @@ let bottoneSomma = document.getElementById("bottone-addizione");
 let bottoneSottrazione = document.getElementById("bottone-sottrazione"); 
 let bottoneDivisione = document.getElementById("bottone-divisione"); 
 let bottoneMoltiplicazione = document.getElementById("bottone-moltiplicazione"); 
+let risultatoAddizione = document.getElementById("risultato-addizione");
+let risultatoSottrazione = document.getElementById("risultato-sottrazione");
+let risultatoDivisione = document.getElementById("risultato-divisione");
+let risultatoMoltiplicazione = document.getElementById("risultato-moltiplicazione");
+
+
+bottoneSomma.addEventListener("click", function() { 
+let somma = parseInt(bottoneNumeroUno.value) + parseInt(bottoneNumeroDue.value)
+risultatoAddizione.textContent = somma; });
 
 
 
