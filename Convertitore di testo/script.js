d4903e133c54820bd03858aa11bf2c3f05282bdb
angelorapana <angelorapana@icloud.com>
2026-01-06 18:10:28 +0100

    creazione funzione per bottone maiuscolo

--- Convertitore di testo/script.js
+++ Convertitore di testo/script.js
@@ -1,6 +1,17 @@
 let testoinserito = document.getElementById("testo");
-let risultatoMaius = \document.getElementById("risultato-maiuscolo");   
+
+let bottoneMaius = document.getElementById("bottone-maiuscolo"); 
+let bottoneMinus = document.getElementById("bottone-minuscolo"); 
+let bottoneInvert = document.getElementById("bottone-inverti"); 
+let bottoneIniziali = document.getElementById("bottone-iniziali"); 
+
+let risultatoMaius = document.getElementById("risultato-maiuscolo");   
 let risultatoMinus = document.getElementById("risultato-minuscolo");
 let risultatoInvert = document.getElementById("risultato-inverti");
 let risultatoIniziali = document.getElementById("risultato-iniziali");
+
+bottoneMaius.addEventListener("click", function() {
+    let testoMaiuscolo = testoinserito.value.toUpperCase();
+    risultatoMaius.innerText = "Risultato: " + testoMaiuscolo;
+});
 
