02ed8d25ec6db5aae42e9bc7ff05e758a1ad9af6
angelorapana <angelorapana@icloud.com>
2026-01-06 18:06:03 +0100

    dichiarazione delle variabili

---
+++ Convertitore di testo/script.js
@@ -0,0 +1,6 @@
+let testoinserito = document.getElementById("testo");
+let risultatoMaius = \document.getElementById("risultato-maiuscolo");   
+let risultatoMinus = document.getElementById("risultato-minuscolo");
+let risultatoInvert = document.getElementById("risultato-inverti");
+let risultatoIniziali = document.getElementById("risultato-iniziali");
+
