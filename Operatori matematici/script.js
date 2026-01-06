3ff5428ac938529701bc01c09363a1c231c24def
angelorapana <angelorapana@icloud.com>
2026-01-06 17:35:47 +0100

    dichiarazione delle variabili

--- Operatori matematici/script.js
+++ Operatori matematici/script.js
@@ -1,10 +1,10 @@
-let bottoneNumeroUno = document.getElementById("bottone-numero-uno");
-let bottoneNumeroDue = document.getElementById("bottone-numero-due");
+let bottoneNumeroUno = document.getElementById("bottone-numero-uno"); 
+let bottoneNumeroDue = document.getElementById("bottone-numero-due"); 
+
+let bottoneSomma = document.getElementById("bottone-addizione"); 
+let bottoneSottrazione = document.getElementById("bottone-sottrazione"); 
+let bottoneDivisione = document.getElementById("bottone-divisione"); 
+let bottoneMoltiplicazione = document.getElementById("bottone-moltiplicazione"); 
 
 
-let bottoneSomma = document.getElementById("bottone-addizione");
 
-bottoneSomma.addEventListener("click", function() {
-    let somma = parseInt(bottoneNumeroUno.value) + parseInt(bottoneNumeroDue.value);
-    alert("La somma è: " + somma);
-});                                                                                             
